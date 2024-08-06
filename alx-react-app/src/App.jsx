c8a29f04f0a7bf66d8fb245fa0b1004f6ac07bc3
WorkuUrga <urgaworku@gmail.com>
2024-08-06 21:02:47 +0300

    corrections

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './WelcomeMessage'
-
 function App() {
   const [count, setCount] = useState(0)
 
