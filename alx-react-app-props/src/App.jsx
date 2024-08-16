99ec83ee919e4b93ef0cbed49c80dcf0515168ef
WorkuUrga <urgaworku@gmail.com>
2024-08-16 09:34:35 +0300

    2. Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
 
+import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import UserProfile from './components/UserProfile.jsx'
+
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
+    <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserContext.Provider value={userData}>
-        <ProfilePage />
+      <ProfilePage />
       </UserContext.Provider>
     </>
   );
---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p style={{textAlign: 'center', fontStyle: 'italic', color: 'lightgray'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
