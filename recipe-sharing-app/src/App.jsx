5c3db6561bf9992c3e8214ff359868f0cea3dc1f
WorkuUrga <urgaworku@gmail.com>
2024-08-20 16:16:25 +0300

    Zustand recipe form

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
