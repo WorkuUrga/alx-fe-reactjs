4fb1ff72cc6be92eeb1dec2fb4e014ee03fa828b
WorkuUrga <urgaworku@gmail.com>
2024-08-20 18:10:55 +0300

    Zustand recipe form

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <RecipeList />
       <AddRecipeForm />
+      <RecipeDetails />
     </>
   )
 }
---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DeleteRecipeButton() {
+  return (
+    <div>DeleteRecipeButton</div>
+  )
+}
+
+export default DeleteRecipeButton
