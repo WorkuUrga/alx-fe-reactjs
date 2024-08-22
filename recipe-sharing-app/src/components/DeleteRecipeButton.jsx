bcce14f9e52a12c05b6d8e89bac90055f1d33968
WorkuUrga <urgaworku@gmail.com>
2024-08-22 09:35:00 +0300

    Zustand recipe form

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import useRecipeStore from './recipeStore'
 
 function DeleteRecipeButton() {
     const deleteRecipe = useRecipeStore(state => state.deleteRecipe)
+    const navigate = useNavigate()
     const handleClick = () => {
         deleteRecipe(recipeId);
+        navigate('/')
     }
     return (
         <div>
