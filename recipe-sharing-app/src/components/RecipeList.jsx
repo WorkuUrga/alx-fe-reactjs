27de49feabf7ac8b59675ef35add0c7854797d68
WorkuUrga <urgaworku@gmail.com>
2024-08-22 10:41:19 +0300

    Zustand recipe form

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import  useRecipeStore  from './recipeStore';
+import React, { useEffect } from 'react';
+import useRecipeStore from './recipeStore';
 
 function RecipeList() {
-    const recipes = useRecipeStore(state => state.recipes);
+    const { recipes, filteredRecipes, searchTerm, setSearchTerm, filterRecipes } = useRecipeStore();
+
+    useEffect(() => {
+        filterRecipes();
+    }, [searchTerm, recipes]); 
+    const recipesToDisplay = searchTerm ? filteredRecipes : recipes;
+
     return (
-    <div>
-        {recipes.map(recipe =>(
-            <div key={recipe.id}>
-                <h3>
-                    <link to ={`/recipe/${recipe.id}`}>{recipe.title}</link>
-                </h3>
-                <p>{recipe.description}</p>
-            </div>
-        ))}
-    </div>
-    )
+        <div>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Search recipes..."
+            />
+            {recipesToDisplay.map(recipe => (
+                <div key={recipe.id}>
+                    <h3>{recipe.title}</h3>
+                    <p>{recipe.description}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default RecipeList
+export default RecipeList;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,16 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
+    })),
 
   addRecipe: (newRecipe) =>
     set((state) => ({
