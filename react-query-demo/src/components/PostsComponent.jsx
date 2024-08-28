2a6a08941ac4816848c67f320153fba8d22118df
WorkuUrga <urgaworku@gmail.com>
2024-08-28 08:34:20 +0300

    Advanced React with form

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {useQuery} from 'react-query'
 
-const fetchData = async () => {
+const fetchPosts = async () => {
     const res = await fetch ('https://jsonplaceholder.typicode.com/posts');
     return res.json();
 };
 
 const PostsComponent = () => {
-    const {data, error, isError, isLoading, refetch} = useQuery('fetchData', fetchData);
+    const {data, error, isError, isLoading, refetch} = useQuery('fetchPosts', fetchPosts);
     if (isLoading) return <div>Loading...</div>
     if(isError) return <div>Error loading data</div>
     return (
