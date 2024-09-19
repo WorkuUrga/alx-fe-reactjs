8d8ccc68f74c8ab0f9fb4352fd90e523c36bd5b1
WorkuUrga <urgaworku@gmail.com>
2024-09-19 10:36:13 +0300

    APIs

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,11 @@
-import UserSearch from './components/UserSearch'
+import Search from './components/Search'
 import './App.css'
 
 function App() {
   return (
     <div className=''>
       <h1>Github User Search</h1>
-      <UserSearch />
+      <Search />
     </div>
   )
 }
