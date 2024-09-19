97c340d0c92e521975ac18bd2043d2a4860bdff2
WorkuUrga <urgaworku@gmail.com>
2024-09-19 10:45:05 +0300

    APIs

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 function UserSearch() {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) =>
+  e.preventDefault()
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
         <input 
         type="text" 
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
         placeholder='Search Github Users...'
         />
         <button>Search</button>
-    </div>
+    </form>
   )
 }
 
